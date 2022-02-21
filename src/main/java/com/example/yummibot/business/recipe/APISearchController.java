3cecf18962518fdb7ea1916a74ae0695fedc3ccd
Mahmoud <mahmouddabdelfattah72@gmail.com>
2022-02-21 16:21:28 +0200

    21-2

--- src/main/java/com/example/yummibot/business/recipe/APISearchController.java
+++ src/main/java/com/example/yummibot/business/recipe/APISearchController.java
@@ -1,48 +1,27 @@
 package com.example.yummibot.business.recipe;
 
 import com.example.yummibot.business.recipe.payload.Recipe;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class APISearchController {
-    String urlForComplexSearch = "https://api.spoonacular.com/recipes/complexSearch";
-    String apikey = "?apiKey=b7f84234d75d4f7ba1de453a856c3fe8";
-    String adhamKey = "?apiKey=21b6a1afab9646f7a125f7ba0331f024";
-    String urlForRandomRecipe = "https://api.spoonacular.com/recipes/random";
-    String youtubeSearchURL = "https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyArfdkorIe3mx059e3qcqLKbETnEx796AA&q=";
+
     private final RecipeRepo recipeRepo;
+    private final SpoonacularClient spoonacularClient;
 
-    public APISearchController(RecipeRepo recipeRepo) {
-        this.recipeRepo = recipeRepo;
-    }
-
-    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder.build();
-    }
 
     @GetMapping("/{name}/{offset}")
     public List<ResultRecipe> Result(@PathVariable(name = "name") String name, @PathVariable(name = "offset") String offset) {
-        RestTemplate restTemplate = restTemplate(new RestTemplateBuilder());
-        return restTemplate.exchange(urlForComplexSearch + apikey + "&query=" + name + "&number=10" + "&offset=" + offset, HttpMethod.GET, setHeaders(), Results.class).getBody().getResults();
+        return spoonacularClient.getRecipes(name,offset);
+//        return Objects.requireNonNull(restTemplate.exchange(urlForComplexSearch + apikey + "&query=" + name + "&number=10" + "&offset=" + offset, HttpMethod.GET, setHeaders(), Results.class).getBody()).getResults();
     }
 
-    public HttpEntity<String> setHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("User-agent", "app");
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        return entity;
-    }
 
     @GetMapping("/getbyid/{id}")
     public Recipe recipeExact(@PathVariable(name = "id") int id) {
@@ -53,26 +32,26 @@ public class APISearchController {
 
     @GetMapping("/belowTwenty/{offset}")
     public List<ResultRecipe> randomRecipe(@PathVariable(name = "offset") String offset) {
-        RestTemplate restTemplate = restTemplate(new RestTemplateBuilder());
-        return restTemplate.exchange(urlForComplexSearch + adhamKey + "&maxReadyTime=20" + "&number=10" + "&offset=" + offset, HttpMethod.GET, setHeaders(), Results.class).getBody().getResults();
+        return spoonacularClient.getRecipesBelowTwenty(offset);
+       // return restTemplate.exchange(urlForComplexSearch + adhamKey + "&maxReadyTime=20" + "&number=10" + "&offset=" + offset, HttpMethod.GET, setHeaders(), Results.class).getBody().getResults();
     }
 
     @GetMapping("/random")
     public List<ResultRecipe> surpriseMe() {
-        RestTemplate restTemplate = restTemplate(new RestTemplateBuilder());
-        return restTemplate.exchange(urlForRandomRecipe + apikey, HttpMethod.GET, setHeaders(), Results.class).getBody().getRecipes();
+        return spoonacularClient.surpriseTheClient();
+      //  return restTemplate.exchange(urlForRandomRecipe + apikey, HttpMethod.GET, setHeaders(), Results.class).getBody().getRecipes();
     }
 
     @GetMapping("/mealType/{mealtype}")
     public List<ResultRecipe> mealType(@PathVariable(name = "mealtype") String mealtype) {
-        RestTemplate restTemplate = restTemplate(new RestTemplateBuilder());
-        return restTemplate.exchange(urlForRandomRecipe + apikey + "&tags=" + mealtype + "&number=10", HttpMethod.GET, setHeaders(), Results.class).getBody().getRecipes();
+       // return restTemplate.exchange(urlForRandomRecipe + apikey + "&tags=" + mealtype + "&number=10", HttpMethod.GET, setHeaders(), Results.class).getBody().getRecipes();
+        return spoonacularClient.getMealType(mealtype);
     }
 
     @GetMapping("/youtuberesults/{recipe}")
     public List<YoutubeSearch> youtubeSearches(@PathVariable(name = "recipe") String recipe) {
-        RestTemplate restTemplate = restTemplate(new RestTemplateBuilder());
-        return restTemplate.exchange(youtubeSearchURL + recipe + " recipe", HttpMethod.GET, setHeaders(), YoutubeResults.class).getBody().getVideoById();
+       // return restTemplate.exchange(youtubeSearchURL + recipe + " recipe&maxResults=3", HttpMethod.GET, setHeaders(), YoutubeResults.class).getBody().getVideoById();
+        return spoonacularClient.getRecipeVideos(recipe);
     }
 
 
---
+++ src/main/java/com/example/yummibot/business/recipe/SpoonacularClient.java
@@ -0,0 +1,140 @@
+package com.example.yummibot.business.recipe;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+public class SpoonacularClient {
+
+    @Value("${spoonacular.api.base-url}")
+    private String baseUrl;
+
+    @Value("${spoonacular.api.complex-search-url}")
+    private String spoonacularComplexSearchUrl;
+
+    @Value("${spoonacular.api.random-recipe-url}")
+    private String spoonacularRandomRecipeUrl;
+
+    @Value("${spoonacular.api.key}")
+    private String spoonacularApiKey;
+
+    @Value("${youtube.api.base-url}")
+    private String youtubeBaseUrl;
+
+    @Value("${youtube.api.search-url}")
+    private String youtubeSearchUrl;
+
+    @Value("${youtube.api.key}")
+    private String youtubeApiKey;
+
+    private final RestTemplate restTemplate;
+
+    private HttpEntity<String> getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("User-agent", "app");
+        return new HttpEntity<>(headers);
+    }
+
+    private String getSpoonacularComplexSearchUrl(String name, String offset) {
+        // https://api.spoonacular.com/recipes/complexSearch21b6a1afab9646f7a125f7ba0331f024&query=Meats&number=1
+        return baseUrl + spoonacularComplexSearchUrl +"?apiKey="+ spoonacularApiKey + "&query=" + name + "&number=10&offset=" + offset;
+    }
+
+    private String getSpoonacularComplexSearchUrlForBelowTwenty(String offset) {
+        return baseUrl + spoonacularComplexSearchUrl +"?apiKey="+ spoonacularApiKey + "&maxReadyTime=20&number=10&offset=" + offset;
+    }
+
+    private String getSpoonacularRandomRecipeUrl() {
+        return baseUrl + spoonacularRandomRecipeUrl + "?apiKey="+ spoonacularApiKey;
+    }
+
+    private String getSpoonacularMealTypeRecipe(String mealType) {
+        return baseUrl + spoonacularRandomRecipeUrl +"?apiKey="+ spoonacularApiKey + "&number=10&tags=" + mealType;
+    }
+
+    private String getYoutubeSearchResult(String recipe) {
+        return youtubeBaseUrl + youtubeSearchUrl + "?key=" + youtubeApiKey + "&q=" + recipe;
+    }
+
+    public List<ResultRecipe> getRecipes(String name, String offset) {
+        ResponseEntity<Results> exchange = restTemplate.exchange(
+                getSpoonacularComplexSearchUrl(name, offset),
+                HttpMethod.GET,
+                getHeaders(),
+                Results.class
+        );
+        if (Objects.isNull(exchange.getBody())) {
+            return new ArrayList<>();
+        } else {
+            return exchange.getBody().getResults();
+        }
+    }
+
+    public List<ResultRecipe> getRecipesBelowTwenty(String offset) {
+        ResponseEntity<Results> exchange = restTemplate.exchange(
+                getSpoonacularComplexSearchUrlForBelowTwenty(offset),
+                HttpMethod.GET,
+                getHeaders(),
+                Results.class
+        );
+        if (Objects.isNull(exchange.getBody())) {
+            return new ArrayList<>();
+        } else {
+            return exchange.getBody().getResults();
+        }
+    }
+
+    public List<ResultRecipe> surpriseTheClient() {
+        ResponseEntity<Results> exchange = restTemplate.exchange(
+                getSpoonacularRandomRecipeUrl(),
+                HttpMethod.GET,
+                getHeaders(),
+                Results.class
+        );
+        if (Objects.isNull(exchange.getBody())) {
+            return new ArrayList<>();
+        } else {
+            return exchange.getBody().getRecipes();
+        }
+    }
+
+    public List<ResultRecipe> getMealType(String mealType) {
+        ResponseEntity<Results> exchange = restTemplate.exchange(
+                getSpoonacularMealTypeRecipe(mealType),
+                HttpMethod.GET,
+                getHeaders(),
+                Results.class
+        );
+        if (Objects.isNull(exchange.getBody())) {
+            return new ArrayList<>();
+        } else {
+            return exchange.getBody().getRecipes();
+        }
+    }
+
+    public List<YoutubeSearch> getRecipeVideos(String recipe) {
+        ResponseEntity<YoutubeResults> exchange = restTemplate.exchange(
+                getYoutubeSearchResult(recipe),
+                HttpMethod.GET,
+                getHeaders(),
+                YoutubeResults.class
+        );
+        if (Objects.isNull(exchange.getBody())) {
+            return new ArrayList<>();
+        } else {
+            return exchange.getBody().getVideoById();
+        }
+    }
+
+}
